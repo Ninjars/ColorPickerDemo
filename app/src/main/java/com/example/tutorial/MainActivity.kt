5dc67c8c6bb42d401b28a54a89eecd99942b053a
Jez <5053926+Ninjars@users.noreply.github.com>
2023-09-08 19:01:12 +0100

    initial implementation of ColorCircle

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tutorial.ui.theme.TutorialTheme
 
@@ -17,12 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ColorCircle(
+                        modifier = Modifier
+                            .fillMaxWidth(1f)
+                            .aspectRatio(1f)
+                    )
                 }
             }
         }
@@ -30,17 +37,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ColorCircle(
+    modifier: Modifier = Modifier,
+) {
+    Canvas(
         modifier = modifier
-    )
+    ) {
+        drawCircle(
+            brush = Brush.sweepGradient(
+                colors = listOf(
+                    Color.Red,
+                    Color.Yellow,
+                    Color.Green,
+                    Color.Cyan,
+                    Color.Blue,
+                    Color.Magenta,
+                    Color.Red
+                )
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.White, Color.Transparent, Color.Black)
+            ),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TutorialTheme {
-        Greeting("Android")
     }
 }
