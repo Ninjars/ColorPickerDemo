cd48ccca1a008d02e99d287b6ff9cc51b09ecc9c
Jez <5053926+Ninjars@users.noreply.github.com>
2023-09-08 19:28:49 +0100

    added slider ui

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tutorial.ui.theme.TutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +29,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ColorCircle(
-                        modifier = Modifier
-                            .fillMaxWidth(1f)
-                            .aspectRatio(1f)
-                    )
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        ColorCircle(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f)
+                        )
+                        Slider(value = 0.25f, onValueChange = { value -> })
+                        Slider(value = 0.5f, onValueChange = { value -> })
+                        Slider(value = 0.75f, onValueChange = { value -> })
+                    }
                 }
             }
         }
